d27f11ed4ae094bd3dd089dc3c2ab46e6e53300f
Benkei Sándor <sanya.benkei@gmail.com>
2020-12-30 17:09:42 +0100

    building cart in progress

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -2,22 +2,22 @@
   <!--v-for="(cartItems,cartIndex) in product[productIndex.index[0]].productType[toCartItemIndex.index[cart.eachIndex]]" :key="cartIndex"-->
 
   <div class="text-gray-200 bg-white bg-gray-800 mb-2"
-  :class="cart.visibility" v-for="(cartItems,cartIndex) in toCartItems[0]" :key="cartIndex">
+  :class="cart.visibility" v-for="(cartItems,cartIndex) in toCartItems" :key="cartIndex">
     <div class="inline-grid grid-cols-4 text-white text-sm w-full">
       <img class="w-auto max-h-24 p-2" :src="cartItems.img" alt="product">
       <div class="text-xs m-auto">{{cartItems.brand}} {{cartItems.name}}</div>
       <!-- counter, increase, decrease -->
       <div class="flex items-center m-auto">
-        <div class="text-lg">{{cart.total}}</div>
+        <div class="text-lg">{{cartItems.total}}</div>
         <div class="flex flex-col items-center h-full ml-3">
           <!-- plus -->
-          <button class="p-1 mb-1 rounded-full bg-gray-700" @click="productPlus">
+          <button class="p-1 mb-1 rounded-full bg-gray-700" @click="productPlus(cartItems,cartIndex)">
             <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd" />
             </svg>
           </button>
           <!-- minus -->
-          <button class="p-1 mt-1 rounded-full bg-gray-700" @click="productMinus" :disabled="cart.minusDisabled">
+          <button class="p-1 mt-1 rounded-full bg-gray-700" @click="productMinus(cartItems,cartIndex)">
             <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M5 10a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1z" clipRule="evenodd" />
             </svg>
@@ -40,15 +40,11 @@ name:'Cart',
 
     // cart dynamic values
     const cart=reactive({
-      visibility:'hidden',
-      total:1,
-      plus:0,
-      minus:0,
-      minusDisabled:false,
+      visibility:'hidden'
     })
 
-      function productPlus(){cart.total+=1}   // add product by clicking
-      function productMinus(){cart.total-=1}  // remove product by clicking
+      function productPlus(cartItems){cartItems.total+=1}   // add product by clicking
+      function productMinus(cartItems){cartItems.total-=1}  // remove product by clicking
     
     onUpdated(()=>{
       //open and close cart
@@ -57,12 +53,8 @@ name:'Cart',
       } else{
         cart.visibility='hidden';
       }
-      // You can't go to negative values on clicking "-" on a product
-      if(cart.total===1){
-        cart.minusDisabled=true
-      } else{
-        cart.minusDisabled=false
-      }
+
+      //if(toCartItems.total===1){cartItems.target.disabled=true} else{cartItems.target.disabled=false}
     })
 
 
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -4,7 +4,7 @@
     <div class="container min-h-screen w-full sm:w-11/12 md:w-10/12 bg-black bg-opacity-75 rounded-t-2xl mx-auto">
       <div class="flex flex-wrap p-5">
         <!-- category card -->
-        <div class="w-9/12 sm:w-6/12 lg:w-4/12 xl:w-3/12 mx-auto my-5" v-for="(card,cardIndex) in product[productIndex.index[0]].productType" :key="cardIndex" @click="openUp(this,cardIndex)">
+        <div class="w-9/12 sm:w-6/12 lg:w-4/12 xl:w-3/12 mx-auto my-5" v-for="(card,cardIndex) in product[productIndex.index[0]].productType" :key="cardIndex">
           <div class="min-h-full flex flex-col items-center bg-gray-200 mx-3 rounded-md cursor-pointer">
             <h3 class="uppercase text-md lg:text-xl text-black text-center border-b border-gray-400 bg-black bg-opacity-10 w-full py-2">{{card.brand}}  {{card.name}}</h3>
             <div class="w-52 h-52">
@@ -13,42 +13,16 @@
             <div class="p-3 text-black">{{card.desc}}</div>
             <div class="flex justify-between items-center w-full p-3">
               <!-- details button -->
-              <button class="flex items-center hover:text-white">
+              <button class="flex items-center hover:text-white" @click="addCart(card,cardIndex)">
                   <a class="border border-gray-400 p-1">
                       <svg class="w-5 h-5 text-red-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
                       </svg>
                   </a>
-                  <p class="ml-2 text-green-800 font-medium">Details</p>
+                  <p class="ml-2 text-green-800 font-medium">Add to cart</p>
               </button>
               <!-- price -->
               <div class="text-right text-green-800 font-semibold text-xl">{{card.price}} EUR</div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <!-- Opened product -->
-      <div class="relative" :class="box.cardVisibility">
-        <div class="fixed transform translate-y-1/2 translate-x-1/2 bottom-1/2 right-1/2 w-80 sm:w-96">
-          <div class="w-full mx-auto my-5">
-            <div class="min-h-full flex flex-col items-center bg-gray-200 mx-3 rounded-md cursor-pointer shadow-outside">
-              <div class="flex items-center pl-2 w-full border-b border-gray-400 bg-black bg-opacity-10">
-                <svg class="text-red-800 w-8 h-auto py-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" @click="exitProduct">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16l-4-4m0 0l4-4m-4 4h18" />
-                </svg>
-                <h3 class="uppercase text-md lg:text-xl text-black font-medium ml-auto pr-4">{{product[productIndex.index[0]].productType[box.selectedCardIndex].brand}} {{product[productIndex.index[0]].productType[box.selectedCardIndex].name}}</h3>
-              </div>
-              <div class="w-52 h-52">
-                  <img class="w-auto h-full m-auto py-4 px-2" :src="product[productIndex.index[0]].productType[box.selectedCardIndex].img" alt="product">
-              </div>
-              <div class="p-3 text-black">{{product[productIndex.index[0]].productType[box.selectedCardIndex].desc}}</div>
-              <div class="flex justify-between items-center w-full p-3">
-                <!-- add to cart button (visible) -->
-                <button class="py-1 px-6 font-semibold text-green-800 border border-green-800 hover:border-red-800 hover:text-red-800" :class="box.showAddButton" @click="addToCart">Buy</button>
-                <!-- price -->
-                <div class="text-right text-green-800 font-semibold text-xl">{{product[productIndex.index[0]].productType[box.selectedCardIndex].price}} EUR</div>
-              </div>
             </div>
           </div>
         </div>
@@ -64,14 +38,13 @@
 <script>
 import useSelect from '@/modules/cars.js';
 import router from '@/router/index.js';
-import { reactive } from 'vue';
+//import { onUpdated } from 'vue';
 
 export default {
 name:'Products',
 
   setup(){
       const { product, productIndex, toCartItems }=useSelect();
-
       // on page refresh it will push you back to car select (home page)
       window.onload=function(){
           router.push('/')
@@ -82,64 +55,26 @@ name:'Products',
               e.brand=`${e.brand} - `
           }
       });
-      // opened product dynamic classes
-      const box=reactive({
-        cardVisibility:'hidden', 
-        showAddButton:'hidden',
-
-        selectedCardIndex:0,   // clicked product index stores here
-      })
       // force to change open product dynamic class states
-      function openUp(cardIndex,index){
-        box.selectedCardIndex=index;  // get clicked product index
-
-        box.cardVisibility='';  
-        box.detailsVisibility='hidden';  
-        box.plusMinusVisibility='flex';
-        box.showAddButton='';
-      }
-      // exit product with "left arrow"
-      function exitProduct(){
-        box.cardVisibility='hidden';
-        box.showAddButton='hidden';
+      function addCart(card){
+        if (toCartItems.includes(card) === false) {
+          card.total=1;
+          toCartItems.push(card)
+        } else{
+          card.total++;
+        }
       }
 
-      const allProduct=[];
-      // add items to cart on click 'Buy'
-      function addToCart(){
-
-        
-        allProduct.push({
-            categoryId:productIndex.index[0],
-            productId:box.selectedCardIndex,
-            brand:product[productIndex.index[0]].productType[box.selectedCardIndex].brand,
-            name:product[productIndex.index[0]].productType[box.selectedCardIndex].name,
-            price:product[productIndex.index[0]].productType[box.selectedCardIndex].price,
-            desc:product[productIndex.index[0]].productType[box.selectedCardIndex].desc,
-            img:product[productIndex.index[0]].productType[box.selectedCardIndex].img,
-        })
-
-        let dataArr = allProduct.map(item=>{
-            return [JSON.stringify(item),item];
-        }); // creates array of array
-        let maparr = new Map(dataArr); // create key value pair from array of array
-        toCartItems.shift();
-        toCartItems.push([...maparr.values()]);
-
-        console.log('to cart: ',toCartItems);
-      }
+      /*onUpdated(()=>{
+        if (condition) {
+          
+        }
+      })*/
 
       return{
           product,
           productIndex,
-          box,
-          openUp,
-          exitProduct,
-          addToCart,
-          toCartItems
-          //item,
-          //productPlus,
-          //productMinus
+          addCart
       }
   }
 }
